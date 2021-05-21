16f413b2b4d1de778776960dfb8b3e67bfaf7d44
Furisto <24721048+Furisto@users.noreply.github.com>
2021-05-21 23:35:58 +0200

    Support multiple subsystems

--- src/cgroups/manager.rs
+++ src/cgroups/manager.rs
@@ -1,67 +1,84 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 use std::{fs::remove_dir, path::Path};
 
 use anyhow::Result;
 use nix::unistd::Pid;
-use procfs::process::{MountInfo, Process};
+use procfs::process::{Process};
 
 use crate::{cgroups::ControllerType, spec::LinuxResources, utils::PathBufExt};
 
-use super::{devices::Devices, Controller};
+use super::{devices::Devices, hugetlb::Hugetlb, Controller};
 
+const CONTROLLERS: &'static [ControllerType] = &[ControllerType::Devices, ControllerType::HugeTlb];
 pub struct Manager {
-    cgroup_path: PathBuf,
-    mount_info: MountInfo,
+    subsystems: HashMap<String, PathBuf>,
 }
 
 impl Manager {
     pub fn new(cgroup_path: PathBuf) -> Result<Self> {
-        let mut mount: Vec<MountInfo> = Process::myself()?
-            .mountinfo()?
-            .into_iter()
-            .filter(|m| {
-                m.fs_type == "cgroup"
-                    && m.mount_point.ends_with(ControllerType::Devices.to_string())
-            })
-            .collect();
+        let mut subsystems = HashMap::<String, PathBuf>::new();
+        for subsystem in CONTROLLERS.into_iter().map(|c|c.to_string()) {
+            subsystems.insert(subsystem.to_owned(),Self::get_subsystem_path(&cgroup_path, &subsystem)?);
+        }
+
         Ok(Manager {
-            cgroup_path,
-            mount_info: mount.pop().unwrap(),
+            subsystems,
         })
     }
 
     pub fn apply(&self, linux_resources: &LinuxResources, pid: Pid) -> Result<()> {
-        let cgroup = Process::myself()?
-            .cgroups()?
-            .into_iter()
-            .filter(|c| c.controllers.contains(&ControllerType::Devices.to_string()))
-            .collect::<Vec<_>>()
-            .pop()
-            .unwrap();
-
-        let p = if self.cgroup_path.to_string_lossy().into_owned().is_empty() {
-            self.mount_info
-                .mount_point
-                .join_absolute_path(Path::new(&cgroup.pathname))?
-        } else {
-            self.mount_info
-                .mount_point
-                .join_absolute_path(&self.cgroup_path)?
-        };
-
-        Devices::apply(linux_resources, &p, pid)?;
+        for subsys in &self.subsystems {
+            match subsys.0.as_str() {
+                "devices" => Devices::apply(linux_resources, &subsys.1, pid)?,
+                "hugetlb" => Hugetlb::apply(linux_resources, &subsys.1, pid)?,
+                _ => continue,
+            }
+        }
 
         Ok(())
     }
 
     pub fn remove(&self) -> Result<()> {
-        let p = self
-            .mount_info
-            .mount_point
-            .join_absolute_path(&self.cgroup_path)?;
-        log::debug!("remove cgroup {:?}", p.display());
-        remove_dir(&p)?;
+        for cgroup_path in &self.subsystems {
+            if cgroup_path.1.exists() {
+                log::debug!("remove cgroup {:?}", cgroup_path.1);
+                remove_dir(&cgroup_path.1)?;
+            }
+        }
 
         Ok(())
+    }
+
+    fn get_subsystem_path(cgroup_path: &Path, subsystem: &str) -> anyhow::Result<PathBuf> {
+        let mount = Process::myself()?
+            .mountinfo()?
+            .into_iter()
+            .filter(|m| {
+                m.fs_type == "cgroup"
+                    && m.mount_point.ends_with(subsystem)
+            })
+            .collect::<Vec<_>>()
+            .pop()
+            .unwrap();
+
+            let cgroup = Process::myself()?
+            .cgroups()?
+            .into_iter()
+            .filter(|c| c.controllers.contains(&subsystem.to_owned()))
+            .collect::<Vec<_>>()
+            .pop()
+            .unwrap();
+
+            let p = if cgroup_path.to_string_lossy().into_owned().is_empty() {
+                    mount
+                    .mount_point
+                    .join_absolute_path(Path::new(&cgroup.pathname))?
+            } else {
+                    mount
+                    .mount_point
+                    .join_absolute_path(&cgroup_path)?
+            };
+
+            Ok(p)
     }
 }
