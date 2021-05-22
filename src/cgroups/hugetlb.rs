6ca2e2145a863ad2d8d048b8744a2975f07ca5c0
Furisto <24721048+Furisto@users.noreply.github.com>
2021-05-22 17:40:52 +0200

    Add integration tests for hugetlb cgroups

--- src/cgroups/hugetlb.rs
+++ src/cgroups/hugetlb.rs
@@ -58,3 +58,56 @@ impl Hugetlb {
         (number != 0) && (number & (number -1)) == 0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+    use crate::spec::LinuxHugepageLimit;
+
+    fn set_fixture(temp_dir: &std::path::Path, filename: &str, val: &str) -> anyhow::Result<()> {
+        std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(temp_dir.join(filename))?
+            .write_all(val.as_bytes())?;
+
+        Ok(())
+    }
+
+    fn create_temp_dir(test_name: &str) -> anyhow::Result<PathBuf> {
+        std::fs::create_dir_all(std::env::temp_dir().join(test_name))?;
+        Ok(std::env::temp_dir().join(test_name))
+    }
+
+    #[test]
+    fn test_set_hugetlb() {
+        let page_file_name = "hugetlb.2MB.limit_in_bytes";
+        let tmp = create_temp_dir("test_set_hugetlb").expect("create temp directory for test");
+        set_fixture(&tmp, page_file_name, "0").expect("Set fixture for 2 MB page size");
+
+        let hugetlb = LinuxHugepageLimit {
+            page_size: "2MB".to_owned(),
+            limit: 16384,
+
+        };
+        Hugetlb::apply(&tmp, &hugetlb).expect("apply hugetlb");
+        let content = std::fs::read_to_string(tmp.join(page_file_name)).expect("Read hugetlb file content");
+        assert_eq!(hugetlb.limit.to_string(), content);
+    }
+
+    #[test]
+    fn test_set_hugetlb_with_invalid_page_size() {
+        let tmp = create_temp_dir("test_set_hugetlb_with_invalid_page_size").expect("create temp directory for test");
+
+        let hugetlb = LinuxHugepageLimit {
+            page_size: "3MB".to_owned(),
+            limit: 16384,
+        };
+
+        let result = Hugetlb::apply(&tmp, &hugetlb);
+        assert!(result.is_err(), "page size that is not a power of two should be an error");
+    }
+}
