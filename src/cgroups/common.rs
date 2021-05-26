db835420466aefa2e65af08ab153380089dc9d91
Furisto <24721048+Furisto@users.noreply.github.com>
2021-05-26 21:32:21 +0200

    Improve file write options

--- src/cgroups/common.rs
+++ src/cgroups/common.rs
@@ -1,10 +1,12 @@
-use std::{fs, io::Write, path::Path};
+use std::{fmt::Debug, fs, io::Write, path::{Path}};
 use nix::sys::statfs;
 
 use anyhow::{Result, anyhow};
 use nix::unistd::Pid;
 
 use oci_spec::LinuxResources;
+
+pub const DEFAULT_CGROUP_ROOT: &str = "/sys/fs/cgroup";
 
 pub trait CgroupManager {
     fn apply(&self, linux_resources: &LinuxResources, pid: Pid) -> Result<()>;
@@ -16,7 +18,7 @@ pub enum Cgroup {
     V2,
 }
 
-pub fn write_cgroup_file(path: &Path, data: &str) -> Result<()> {
+pub fn write_cgroup_file_truncate(path: &Path, data: &str) -> Result<()> {
     fs::OpenOptions::new()
     .create(false)
     .write(true)
@@ -27,8 +29,19 @@ pub fn write_cgroup_file(path: &Path, data: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn detect_cgroup_version(path: &Path) -> Result<Cgroup> {
-    let statfs = statfs::statfs(path)?;
+pub fn write_cgroup_file(path: &Path, data: &str) -> Result<()> {
+    fs::OpenOptions::new()
+    .create(false)
+    .write(true)
+    .truncate(false)
+    .open(path)?
+    .write_all(data.as_bytes())?;
+
+    Ok(())
+}
+
+pub fn detect_cgroup_version<P: AsRef<Path> + Debug>(path: P) -> Result<Cgroup> {
+    let statfs = statfs::statfs(path.as_ref())?;
     let cgroup_type = match statfs.filesystem_type() {
         statfs::CGROUP_SUPER_MAGIC => Cgroup::V1,
         statfs::CGROUP2_SUPER_MAGIC => Cgroup::V2,
