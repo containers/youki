f6b489f9012b06fdc48da7a9453eb12bb43e450b
Furisto <24721048+Furisto@users.noreply.github.com>
2021-05-26 21:33:02 +0200

    Implement string trait for controller types

--- src/cgroups/v2/controller_type.rs
+++ src/cgroups/v2/controller_type.rs
@@ -1,8 +1,21 @@
 pub enum ControllerType {
     Cpu,
     CpuSet,
-    Memory,
     IO,
+    Memory,
     HugeTlb,
     Pids,
+}
+
+impl ToString for ControllerType {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Cpu => "cpu".into(),
+            Self::CpuSet => "cpuset".into(),
+            Self::IO => "io".into(),
+            Self::Memory => "memory".into(),
+            Self::HugeTlb => "hugetlb".into(),
+            Self::Pids => "pids".into(),
+        }
+    }
 }
