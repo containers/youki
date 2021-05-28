40fbc6136427f881551262971c5cb4fa0f4db080
Furisto <24721048+Furisto@users.noreply.github.com>
2021-05-28 18:41:32 +0200

    Add tests for cpu controller

--- src/cgroups/test.rs
+++ src/cgroups/test.rs
@@ -1,18 +1,74 @@
+#![cfg(test)]
+
 use std::{io::Write, path::{Path, PathBuf}};
 use anyhow::Result;
 
-fn set_fixture(temp_dir: &Path, filename: &str, val: &str) -> Result<()> {
+use crate::spec::LinuxCpu;
+
+
+pub fn set_fixture(temp_dir: &Path, filename: &str, val: &str) -> Result<PathBuf> {
+    let full_path = temp_dir.join(filename);
+
     std::fs::OpenOptions::new()
         .create(true)
         .write(true)
         .truncate(true)
-        .open(temp_dir.join(filename))?
+        .open(&full_path)?
         .write_all(val.as_bytes())?;
 
-    Ok(())
+    Ok(full_path)
 }
 
-fn create_temp_dir(test_name: &str) -> Result<PathBuf> {
+pub fn create_temp_dir(test_name: &str) -> Result<PathBuf> {
     std::fs::create_dir_all(std::env::temp_dir().join(test_name))?;
     Ok(std::env::temp_dir().join(test_name))
+}
+
+pub struct LinuxCpuBuilder {
+    resource: LinuxCpu,
+}
+
+impl LinuxCpuBuilder {
+    pub fn new() -> Self {
+        Self {
+            resource: LinuxCpu {
+                shares: None,
+                quota: None,
+                period: None,
+                realtime_runtime: None,
+                realtime_period: None,
+                cpus: None,
+                mems: None,
+            }
+        }
+    }
+
+    pub fn with_shares(mut self, shares: u64) -> Self {
+        self.resource.shares = Some(shares);
+        self
+    }
+
+    pub fn with_quota(mut self, quota: i64) -> Self {
+        self.resource.quota = Some(quota);
+        self
+    }
+
+    pub fn with_period(mut self, period: u64) -> Self {
+        self.resource.period = Some(period);
+        self
+    }
+
+    pub fn with_realtime_runtime(mut self, runtime: i64) -> Self {
+        self.resource.realtime_runtime = Some(runtime);
+        self
+    }
+
+    pub fn with_realtime_period(mut self, period: u64) -> Self {
+        self.resource.realtime_period = Some(period);
+        self
+    }
+
+    pub fn build(self) -> LinuxCpu {
+        self.resource
+    }
 }
