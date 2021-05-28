873e785e02f0fd5f134e6cd347c2bc968adcfb62
Furisto <24721048+Furisto@users.noreply.github.com>
2021-05-28 22:37:43 +0200

    Use Result consistently

--- src/cgroups/v2/hugetlb.rs
+++ src/cgroups/v2/hugetlb.rs
@@ -1,10 +1,12 @@
+use anyhow::Result;
+
 use super::controller::Controller;
 use oci_spec::LinuxResources;
 
 pub struct HugeTlb {}
 
 impl Controller for HugeTlb {
-    fn apply(linux_resources: &LinuxResources, cgroup_path: &std::path::Path) -> anyhow::Result<()> {
+    fn apply(linux_resources: &LinuxResources, cgroup_path: &std::path::Path) -> Result<()> {
         Ok(())
     }
 }
--- src/cgroups/v2/pids.rs
+++ src/cgroups/v2/pids.rs
@@ -1,10 +1,12 @@
+use anyhow::Result;
+
 use super::controller::Controller;
 use oci_spec::LinuxResources;
 
 pub struct Pids {}
 
 impl Controller for Pids {
-    fn apply(linux_resources: &LinuxResources, cgroup_path: &std::path::Path) -> anyhow::Result<()> {
+    fn apply(linux_resources: &LinuxResources, cgroup_path: &std::path::Path) -> Result<()> {
         Ok(())
     }
 }
