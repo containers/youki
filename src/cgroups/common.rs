bf6e3b8324e5f99082c37f461ab294d513f25000
Furisto <24721048+Furisto@users.noreply.github.com>
2021-05-30 16:24:08 +0200

    Control selection of cgroup fs with env variable

--- src/cgroups/common.rs
+++ src/cgroups/common.rs
@@ -1,5 +1,5 @@
-use nix::sys::statfs;
 use std::{
+    env,
     fmt::{Debug, Display},
     fs,
     io::Write,
@@ -60,58 +60,44 @@ pub fn write_cgroup_file(path: &Path, data: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn detect_cgroup_version<P: AsRef<Path> + Debug>(path: P) -> Result<Option<Cgroup>> {
-    let statfs = statfs::statfs(path.as_ref())?;
-    let cgroup_version = match statfs.filesystem_type() {
-        statfs::CGROUP_SUPER_MAGIC => Some(Cgroup::V1),
-        statfs::CGROUP2_SUPER_MAGIC => Some(Cgroup::V2),
-        _ => None,
-    };
-
-    Ok(cgroup_version)
-}
-
 pub fn create_cgroup_manager<P: Into<PathBuf>>(cgroup_path: P) -> Result<Box<dyn CgroupManager>> {
-    // first try the usual cgroup fs location
-    // let root_cgroup_path = PathBuf::from(DEFAULT_CGROUP_ROOT);
-    // if root_cgroup_path.exists() {
-    //     if let Some(cgroup_version) = detect_cgroup_version(&root_cgroup_path)? {
-    //         log::info!("cgroup manager {} will be used", cgroup_version);
-    //         let manager: Box<dyn CgroupManager> = match cgroup_version {
-    //             Cgroup::V1 => Box::new(v1::manager::Manager::new(cgroup_path.into())?),
-    //             Cgroup::V2 => Box::new(v2::manager::Manager::new(
-    //                 root_cgroup_path,
-    //                 cgroup_path.into(),
-    //             )?),
-    //         };
-
-    //         return Ok(manager);
-    //     }
-    // }
-
-    // try to find it from the mountinfo
-    let mount = Process::myself()?
+    let cgroup_mount = Process::myself()?
         .mountinfo()?
         .into_iter()
         .find(|m| m.fs_type == "cgroup");
 
-    if let Some(_) = mount {
-        log::info!("cgroup manager V1 will be used");
-        return Ok(Box::new(v1::manager::Manager::new(cgroup_path.into())?));
-    }
-
-    let mount = Process::myself()?
+    let cgroup2_mount = Process::myself()?
         .mountinfo()?
         .into_iter()
         .find(|m| m.fs_type == "cgroup2");
 
-    if let Some(cgroup2) = mount {
-        log::info!("cgroup manager V2 will be used");
-        return Ok(Box::new(v2::manager::Manager::new(
-            cgroup2.mount_point,
-            cgroup_path.into(),
-        )?));
+    match (cgroup_mount, cgroup2_mount) {
+        (Some(_), None) => {
+            log::info!("cgroup manager V1 will be used");
+            return Ok(Box::new(v1::manager::Manager::new(cgroup_path.into())?));
+        }
+        (None, Some(cgroup2)) => {
+            log::info!("cgroup manager V2 will be used");
+            return Ok(Box::new(v2::manager::Manager::new(
+                cgroup2.mount_point,
+                cgroup_path.into(),
+            )?));
+        }
+        (Some(_), Some(cgroup2)) => {
+            let cgroup_override = env::var("YOUKI_PREFER_CGROUPV2");
+            match cgroup_override {
+                Ok(v) if v == "true" => {
+                    log::info!("cgroup manager V2 will be used");
+                    return Ok(Box::new(v2::manager::Manager::new(
+                        cgroup2.mount_point,
+                        cgroup_path.into(),
+                    )?));
+                }
+                _ => {
+                    return Ok(Box::new(v1::manager::Manager::new(cgroup_path.into())?))
+                }
+            }
+        }
+        _ => return Err(anyhow!("could not find cgroup filesystem")),
     }
-
-    Err(anyhow!("could not find cgroup filesystem"))
 }
