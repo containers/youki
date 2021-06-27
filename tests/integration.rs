3e6e899a6f229a0a229a336ef78a09836f8a82d2
oliver <minakawa.daiki.as@gmail.com>
2021-06-28 04:26:17 +0900

    Implement tests to check the container lifecycle

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,19 +1,60 @@
-use std::env;
-use std::path::PathBuf;
-use std::process::{Command, Stdio};
+#[cfg(test)]
+mod integration {
+    mod support;
+    mod create;
+    mod start;
+    mod state;
+    mod kill;
+    mod delete;
 
-#[test]
-fn main() {
-    let current_dir_path_result = env::current_dir();
-    let current_dir_path = match current_dir_path_result {
-        Ok(path_buf) => path_buf,
-        Err(_) => panic!("directory is not found"),
-    };
-    let youki_path = current_dir_path.join(PathBuf::from("youki"));
-    let status = Command::new(youki_path)
-        .stdout(Stdio::null())
-        .arg("-h")
-        .status()
-        .expect("failed to execute process");
-    assert!(status.success());
+    // This tests the entire lifecycle of the container.
+    #[test]
+    fn valid_life_cycle() {
+        let project_path = support::create_project_path();
+        let uuid = support::generate_uuid();
+        if  support::initialize_test(&project_path).is_err() {
+            panic!("Can not initilize test.");
+        }
+
+        if !create::exec(&project_path, &uuid.to_string()) {
+            if support::cleanup_test(&project_path).is_err() {
+                panic!("Can not cleanup test.");
+            }
+            panic!("This operation must create a new container.");
+        }
+        if !state::exec(&project_path, &uuid.to_string()) {
+            if support::cleanup_test(&project_path).is_err() {
+                panic!("Can not cleanup test.");
+            }
+            panic!("This operation must state the container.");
+        }
+        if !start::exec(&project_path, &uuid.to_string()) {
+            if support::cleanup_test(&project_path).is_err() {
+                panic!("Can not cleanup test.");
+            }
+            panic!("This operation must start the container.");
+        }
+        if !state::exec(&project_path, &uuid.to_string()) {
+            if support::cleanup_test(&project_path).is_err() {
+                panic!("Can not cleanup test.");
+            }
+            panic!("This operation must state the container.");
+        }
+        if !kill::exec(&project_path, &uuid.to_string()) {
+            if support::cleanup_test(&project_path).is_err() {
+                panic!("Can not cleanup test.");
+            }
+            panic!("This operation must kill the container.");
+        }
+        if !delete::exec(&project_path, &uuid.to_string()) {
+            if support::cleanup_test(&project_path).is_err() {
+                panic!("Can not cleanup test.");
+            }
+            panic!("This operation must delete the container.");
+        }
+
+        if support::cleanup_test(&project_path).is_err() {
+            panic!("Can not cleanup test.");
+        }
+    }
 }
