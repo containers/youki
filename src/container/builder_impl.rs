de3bb1ff5a27ffa837b38f7b48bb420861dda1d3
yihuaf <yihuaf@unkies.org>
2021-07-19 05:06:27 +0000

    implement container creation using clone

--- src/container/builder_impl.rs
+++ src/container/builder_impl.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{fs, io::Write, path::PathBuf};
 
 use anyhow::{Context, Result};
 use nix::{
@@ -8,11 +8,12 @@ use nix::{
 use oci_spec::Spec;
 
 use crate::{
-    cgroups,
+    capabilities, cgroups,
     command::{linux::LinuxSyscall, Syscall},
     namespaces::Namespaces,
     notify_socket::NotifyListener,
-    process::{fork, setup_init_process, Process},
+    process::{child, fork, parent},
+    rootfs,
     rootless::Rootless,
     stdio::FileDescriptor,
     tty, utils,
@@ -55,6 +56,8 @@ impl ContainerBuilderImpl {
                 std::process::exit(0);
             }
         }
+
+        self.run_container_clone()?;
 
         Ok(())
     }
@@ -142,4 +145,153 @@ impl ContainerBuilderImpl {
             _ => unreachable!(),
         }
     }
+
+    fn run_container_clone(&mut self) -> Result<()> {
+        prctl::set_dumpable(false).unwrap();
+
+        let linux = self.spec.linux.as_ref().unwrap();
+        let cgroups_path = utils::get_cgroup_path(&linux.cgroups_path, &self.container_id);
+        let cmanager = cgroups::common::create_cgroup_manager(&cgroups_path, self.use_systemd)?;
+        let namespaces: Namespaces = linux.namespaces.clone().into();
+
+        // create the parent and child process structure so the parent and child process can sync with each other
+        let (mut parent, parent_channel) = parent::ParentProcess::new(self.rootless.clone())?;
+        let mut child = child::ChildProcess::new(parent_channel)?;
+
+        let cb = Box::new(|| {
+            if let Err(error) = container_init(
+                self.rootless.clone(),
+                self.spec.clone(),
+                self.syscall.clone(),
+                self.rootfs.clone(),
+                self.console_socket.clone(),
+                self.container_dir.clone(),
+                &mut child,
+            ) {
+                log::debug!("failed to run container_init: {:?}", error);
+                return -1;
+            }
+
+            0
+        });
+
+        let init_pid = fork::clone(cb, namespaces.clone_flags)?;
+        log::debug!("init pid is {:?}", init_pid);
+
+        parent.wait_for_child_ready(init_pid)?;
+
+        if self.rootless.is_none() && linux.resources.is_some() {
+            cmanager.add_task(init_pid)?;
+            cmanager.apply(&linux.resources.as_ref().unwrap())?;
+        }
+
+        // if file to write the pid to is specified, write pid of the child
+        if let Some(pid_file) = &self.pid_file {
+            fs::write(&pid_file, format!("{}", init_pid))?;
+        }
+
+        if let Some(container) = &self.container {
+            // update status and pid of the container process
+            container
+                .update_status(ContainerStatus::Created)
+                .set_creator(nix::unistd::geteuid().as_raw())
+                .set_pid(init_pid.as_raw())
+                .save()?;
+        }
+
+        Ok(())
+    }
+}
+
+fn container_init(
+    rootless: Option<Rootless>,
+    spec: Spec,
+    command: LinuxSyscall,
+    rootfs: PathBuf,
+    console_socket: Option<FileDescriptor>,
+    container_dir: PathBuf,
+    child: &mut child::ChildProcess,
+) -> Result<()> {
+    let linux = spec.linux.as_ref().unwrap();
+    let namespaces: Namespaces = linux.namespaces.clone().into();
+    // need to create the notify socket before we pivot root, since the unix
+    // domain socket used here is outside of the rootfs of container
+    let mut notify_socket: NotifyListener = NotifyListener::new(&container_dir)?;
+    let proc = &spec.process;
+
+    // if Out-of-memory score adjustment is set in specification.  set the score
+    // value for the current process check
+    // https://dev.to/rrampage/surviving-the-linux-oom-killer-2ki9 for some more
+    // information
+    if let Some(ref resource) = linux.resources {
+        if let Some(oom_score_adj) = resource.oom_score_adj {
+            let mut f = fs::File::create("/proc/self/oom_score_adj")?;
+            f.write_all(oom_score_adj.to_string().as_bytes())?;
+        }
+    }
+
+    // if new user is specified in specification, this will be true and new
+    // namespace will be created, check
+    // https://man7.org/linux/man-pages/man7/user_namespaces.7.html for more
+    // information
+    if rootless.is_some() {
+        // child needs to be dumpable, otherwise the non root parent is not
+        // allowed to write the uid/gid maps
+        prctl::set_dumpable(true).unwrap();
+        child.request_identifier_mapping()?;
+        child.wait_for_mapping_ack()?;
+        prctl::set_dumpable(false).unwrap();
+    }
+
+    // set limits and namespaces to the process
+    for rlimit in spec.process.rlimits.iter() {
+        command.set_rlimit(rlimit)?
+    }
+
+    command.set_id(Uid::from_raw(0), Gid::from_raw(0))?;
+
+    // set up tty if specified
+    if let Some(csocketfd) = console_socket {
+        tty::setup_console(&csocketfd)?;
+    }
+
+    // join existing namespaces
+    namespaces.apply_setns()?;
+
+    command.set_hostname(spec.hostname.as_str())?;
+
+    if proc.no_new_privileges {
+        let _ = prctl::set_no_new_privileges(true);
+    }
+
+    rootfs::prepare_rootfs(
+        &spec,
+        &rootfs,
+        namespaces
+            .clone_flags
+            .contains(sched::CloneFlags::CLONE_NEWUSER),
+    )?;
+
+    // change the root of filesystem of the process to the rootfs
+    command.pivot_rootfs(&rootfs)?;
+
+    command.set_id(Uid::from_raw(proc.user.uid), Gid::from_raw(proc.user.gid))?;
+    capabilities::reset_effective(&command)?;
+    if let Some(caps) = &proc.capabilities {
+        capabilities::drop_privileges(&caps, &command)?;
+    }
+
+    // notify parents that the init process is ready to execute the payload.
+    child.notify_parent()?;
+
+    // listing on the notify socket for container start command
+    notify_socket.wait_for_container_start()?;
+
+    let args: &Vec<String> = &spec.process.args;
+    let envs: &Vec<String> = &spec.process.env;
+    utils::do_exec(&args[0], args, envs)?;
+
+    // After do_exec is called, the process is replaced with the container
+    // payload through execvp, so it should never reach here.
+    unreachable!();
 }
