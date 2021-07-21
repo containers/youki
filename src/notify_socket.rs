49ed25ef7115a1d6b409e1eac9f0277977ec0e0e
yihuaf <yihuaf@unkies.org>
2021-07-21 01:42:36 +0000

    Fix unix domain socket may be longer than 108b.

--- src/notify_socket.rs
+++ src/notify_socket.rs
@@ -4,7 +4,7 @@ use std::os::unix::io::AsRawFd;
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::{Path, PathBuf};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use nix::unistd::{self, close};
 
 pub const NOTIFY_FILE: &str = "notify.sock";
@@ -14,8 +14,25 @@ pub struct NotifyListener {
 }
 
 impl NotifyListener {
-    pub fn new(socket_name: &Path) -> Result<Self> {
-        let stream = UnixListener::bind(socket_name)?;
+    pub fn new(socket_path: &Path) -> Result<Self> {
+        // unix domain socket has a maximum length of 108, different from
+        // normal path length of 255. due to how docker create the path name
+        // to the container working directory, there is a high chance that
+        // the full absolute path is over the limit. to work around this
+        // limitation, we chdir first into the workdir where the socket is,
+        // and chdir back after the socket is created.
+        let workdir = socket_path.parent().unwrap();
+        let socket_name = socket_path.file_name().unwrap();
+        let cwd = unistd::getcwd().context("Failed to get cwd")?;
+        unistd::chdir(workdir).context(format!(
+            "Failed to chdir into {}",
+            workdir.to_str().unwrap()
+        ))?;
+        let stream = UnixListener::bind(socket_name)
+            .context(format!("Failed to bind {}", socket_name.to_str().unwrap()))?;
+        unistd::chdir(&cwd)
+            .context(format!("Failed to chdir back to {}", cwd.to_str().unwrap()))?;
+
         Ok(Self { socket: stream })
     }
 
