ad8e41725b1ed0331f81e688def50b8d75de1dca
yihuaf <yihuaf@unkies.org>
2021-08-23 22:56:22 +0200

    Remove the use of epoll.
    Use blocking mio pipe directly.

--- src/process/channel.rs
+++ src/process/channel.rs
@@ -1,42 +1,28 @@
 use crate::process::message::Message;
 use anyhow::bail;
+use anyhow::Context;
 use anyhow::Result;
 use mio::unix::pipe;
 use mio::unix::pipe::{Receiver, Sender};
-use mio::{Events, Interest, Poll, Token};
 use nix::unistd::Pid;
-use std::io::ErrorKind;
 use std::io::Read;
 use std::io::Write;
-use std::time::Duration;
-
-/// Maximum event capacity of polling
-const MAX_EVENTS: usize = 128;
-/// Time to wait when polling for message from child process
-const WAIT_FOR_CHILD: Duration = Duration::from_secs(5);
-/// Time to wait when polling for mapping ack from parent
-const WAIT_FOR_MAPPING: Duration = Duration::from_secs(3);
-// Token is used to identify which socket generated an event
-const PARENT: Token = Token(0);
 
 pub struct Channel {
     sender: Sender,
     receiver: Receiver,
-    poll: Poll,
 }
 
 impl Channel {
     pub fn new() -> Result<Self> {
-        let poll = Poll::new()?;
-        let (sender, mut receiver) = pipe::new()?;
-        poll.registry()
-            .register(&mut receiver, PARENT, Interest::READABLE)?;
-
-        Ok(Self {
-            sender,
-            receiver,
-            poll,
-        })
+        let (sender, receiver) = pipe::new()?;
+        // Our use case is for the process to wait for the communication to come
+        // through, so we set nonblocking to false here (double negative). It is
+        // expected that the waiting process will block and wait.
+        receiver
+            .set_nonblocking(false)
+            .with_context(|| "Failed to set channel receiver to blocking")?;
+        Ok(Self { sender, receiver })
     }
 
     pub fn send_child_ready(&mut self, pid: Pid) -> Result<()> {
@@ -64,119 +50,60 @@ impl Channel {
 
     // wait until the parent process has finished writing the id mappings
     pub fn wait_for_mapping_ack(&mut self) -> Result<()> {
-        let mut events = Events::with_capacity(MAX_EVENTS);
         log::debug!("waiting for mapping ack");
+        let mut buf = [0; 1];
+        self.receiver
+            .read_exact(&mut buf)
+            .with_context(|| "Failed to receive a message from the child process.")?;
 
-        self.poll.poll(&mut events, Some(WAIT_FOR_MAPPING))?;
-        for event in events.iter() {
-            if event.token() == PARENT {
-                let mut buf = [0; 1];
-                if let Err(e) = self.receiver.read_exact(&mut buf) {
-                    if e.kind() != ErrorKind::WouldBlock {
-                        bail!(
-                            "Failed to receive a message from the child process. {:?}",
-                            e
-                        )
-                    }
-                }
-
-                match Message::from(u8::from_be_bytes(buf)) {
-                    Message::MappingWritten => return Ok(()),
-                    msg => bail!(
-                        "receive unexpected message {:?} in waiting for mapping ack",
-                        msg
-                    ),
-                }
-            } else {
-                unreachable!();
-            }
+        match Message::from(u8::from_be_bytes(buf)) {
+            Message::MappingWritten => Ok(()),
+            msg => bail!(
+                "receive unexpected message {:?} in waiting for mapping ack",
+                msg
+            ),
         }
-
-        unreachable!("timed out waiting for mapping ack")
     }
 
     pub fn wait_for_mapping_request(&mut self) -> Result<()> {
-        // Create collection with capacity to store up to MAX_EVENTS events
-        let mut events = Events::with_capacity(MAX_EVENTS);
-        loop {
-            // poll the receiving end of pipe created for WAIT_FOR_CHILD duration for an event
-            self.poll.poll(&mut events, Some(WAIT_FOR_CHILD))?;
-            for event in events.iter() {
-                if event.token() == PARENT {
-                    // read data from pipe
-                    let mut buf = [0; 1];
-                    if let Err(e) = self.receiver.read_exact(&mut buf) {
-                        if e.kind() != ErrorKind::WouldBlock {
-                            bail!(
-                                "Failed to receive a message from the child process. {:?}",
-                                e
-                            )
-                        }
-                    }
+        let mut buf = [0; 1];
+        self.receiver
+            .read_exact(&mut buf)
+            .with_context(|| "Failed to receive a message from the child process.")?;
 
-                    // convert to Message wrapper
-                    match Message::from(u8::from_be_bytes(buf)) {
-                        Message::WriteMapping => {
-                            return Ok(());
-                        }
-                        msg => bail!(
-                            "receive unexpected message {:?} waiting for mapping request",
-                            msg
-                        ),
-                    }
-                } else {
-                    unreachable!();
-                }
-            }
+        // convert to Message wrapper
+        match Message::from(u8::from_be_bytes(buf)) {
+            Message::WriteMapping => Ok(()),
+            msg => bail!(
+                "receive unexpected message {:?} waiting for mapping request",
+                msg
+            ),
         }
     }
 
     /// Waits for associated child process to send ready message
     /// and return the pid of init process which is forked by child process
     pub fn wait_for_child_ready(&mut self) -> Result<Pid> {
-        // Create collection with capacity to store up to MAX_EVENTS events
-        let mut events = Events::with_capacity(MAX_EVENTS);
-        loop {
-            // poll the receiving end of pipe created for WAIT_FOR_CHILD duration for an event
-            self.poll.poll(&mut events, Some(WAIT_FOR_CHILD))?;
-            for event in events.iter() {
-                if event.token() == PARENT {
-                    // read data from pipe
-                    let mut buf = [0; 1];
-                    if let Err(e) = self.receiver.read_exact(&mut buf) {
-                        if e.kind() != ErrorKind::WouldBlock {
-                            bail!(
-                                "Failed to receive a message from the child process. {:?}",
-                                e
-                            )
-                        }
-                    }
+        let mut buf = [0; 1];
+        self.receiver
+            .read_exact(&mut buf)
+            .with_context(|| "Failed to receive a message from the child process.")?;
 
-                    // convert to Message wrapper
-                    match Message::from(u8::from_be_bytes(buf)) {
-                        Message::ChildReady => {
-                            log::debug!("received child ready message");
-                            let mut buf = [0; 4];
-                            if let Err(e) = self.receiver.read_exact(&mut buf) {
-                                if e.kind() != ErrorKind::WouldBlock {
-                                    bail!(
-                                        "Failed to receive a message from the child process. {:?}",
-                                        e
-                                    )
-                                }
-                            }
+        match Message::from(u8::from_be_bytes(buf)) {
+            Message::ChildReady => {
+                log::debug!("received child ready message");
+                // Read the Pid which will be i32 or 4 bytes.
+                let mut buf = [0; 4];
+                self.receiver
+                    .read_exact(&mut buf)
+                    .with_context(|| "Failed to receive a message from the child process.")?;
 
-                            return Ok(Pid::from_raw(i32::from_be_bytes(buf)));
-                        }
-                        msg => bail!(
-                            "receive unexpected message {:?} waiting for child ready",
-                            msg
-                        ),
-                    }
-                } else {
-                    unreachable!();
-                }
+                Ok(Pid::from_raw(i32::from_be_bytes(buf)))
             }
+            msg => bail!(
+                "receive unexpected message {:?} waiting for child ready",
+                msg
+            ),
         }
     }
 
@@ -190,6 +117,7 @@ impl Channel {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::Context;
     use nix::sys::wait;
     use nix::unistd;
 
@@ -198,13 +126,16 @@ mod tests {
         let ch = &mut Channel::new()?;
         match unsafe { unistd::fork()? } {
             unistd::ForkResult::Parent { child } => {
-                wait::waitpid(child, None)?;
-                let pid = ch.wait_for_child_ready()?;
+                let pid = ch
+                    .wait_for_child_ready()
+                    .with_context(|| "Failed to wait for child ready")?;
                 assert_eq!(pid, child);
+                wait::waitpid(child, None)?;
             }
             unistd::ForkResult::Child => {
                 let pid = unistd::getpid();
-                ch.send_child_ready(pid)?;
+                ch.send_child_ready(pid)
+                    .with_context(|| "Failed to send child ready")?;
                 std::process::exit(0);
             }
         };
@@ -217,11 +148,13 @@ mod tests {
         let ch = &mut Channel::new()?;
         match unsafe { unistd::fork()? } {
             unistd::ForkResult::Parent { child } => {
-                ch.wait_for_mapping_ack()?;
+                ch.wait_for_mapping_ack()
+                    .with_context(|| "Failed to wait for mapping ack")?;
                 wait::waitpid(child, None)?;
             }
             unistd::ForkResult::Child => {
-                ch.send_mapping_written()?;
+                ch.send_mapping_written()
+                    .with_context(|| "Failed to send mapping written")?;
                 std::process::exit(0);
             }
         };
