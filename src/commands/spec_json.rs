8dda74e1d8c6aa9d46df1d7e61f6eef4521fa979
yukang <moorekang@gmail.com>
2021-08-27 15:16:41 +0000

    add rootless option for spec

--- src/commands/spec_json.rs
+++ src/commands/spec_json.rs
@@ -6,13 +6,20 @@ use std::fs::File;
 
 /// Command generates a config.json
 #[derive(Clap, Debug)]
-pub struct SpecJson;
+pub struct SpecJson {
+    /// Generate a configuration for a rootless container
+    #[clap(long)]
+    pub rootless: bool,
+}
 
 /// spec Cli command
 impl SpecJson {
     pub fn exec(&self) -> Result<()> {
         // get default values for Spec
-        let default_json: Spec = Default::default();
+        let mut default_json: Spec = Default::default();
+        if self.rootless {
+            default_json.set_for_rootless()?;
+        }
         // write data to config.json
         to_writer_pretty(&File::create("config.json")?, &default_json)?;
         Ok(())
