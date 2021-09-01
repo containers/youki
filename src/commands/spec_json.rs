c81347fb5aee4c4b8d23fa75af3139c9f82f3ae6
yukang <moorekang@gmail.com>
2021-09-01 18:54:52 +0800

    add rootless parameters for default

--- src/commands/spec_json.rs
+++ src/commands/spec_json.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
 use clap::Clap;
+use nix;
 use oci_spec::Spec;
 use serde_json::to_writer_pretty;
 use std::fs::File;
-
 /// Command generates a config.json
 #[derive(Clap, Debug)]
 pub struct SpecJson {
@@ -18,7 +18,10 @@ impl SpecJson {
         // get default values for Spec
         let mut default_json: Spec = Default::default();
         if self.rootless {
-            default_json.set_for_rootless()?;
+            default_json.set_for_rootless(
+                nix::unistd::geteuid().as_raw(),
+                nix::unistd::getegid().as_raw(),
+            )?;
         }
         // write data to config.json
         to_writer_pretty(&File::create("config.json")?, &default_json)?;
