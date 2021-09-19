24c6dae53618fec58c630fb9a77976588cc739a5
utam0k <k0ma@utam0k.jp>
2021-09-19 16:44:20 +0900

    handle name as a str instead of a String.

--- test_framework/src/test_manager.rs
+++ test_framework/src/test_manager.rs
@@ -4,7 +4,7 @@ use std::collections::BTreeMap;
 
 /// This manages all test groups, and thus the tests
 pub struct TestManager<'a> {
-    test_groups: BTreeMap<String, &'a dyn TestableGroup>,
+    test_groups: BTreeMap<&'a str, &'a dyn TestableGroup<'a>>,
 }
 
 impl<'a> Default for TestManager<'a> {
@@ -22,13 +22,13 @@ impl<'a> TestManager<'a> {
     }
 
     /// add a test group to the test manager
-    pub fn add_test_group(&mut self, tg: &'a dyn TestableGroup) {
+    pub fn add_test_group(&mut self, tg: &'a dyn TestableGroup<'a>) {
         self.test_groups.insert(tg.get_name(), tg);
     }
 
     /// Prints the given test results, usually used to print
     /// results of a test group
-    fn print_test_result(&self, name: &str, res: Vec<(&String, &TestResult)>) {
+    fn print_test_result(&self, name: &str, res: &[(&'a str, TestResult)]) {
         println!("# Start group {}", name);
         let len = res.len();
         for (idx, (name, res)) in res.iter().enumerate() {
@@ -49,13 +49,9 @@ impl<'a> TestManager<'a> {
     }
 
     /// Run all tests from given group
-    fn run_test_group(&self, name: &str, tg: &'a dyn TestableGroup) {
+    fn run_test_group(&self, name: &str, tg: &'a dyn TestableGroup<'a>) {
         let results = tg.run_all();
-        let mut test_vec = Vec::new();
-        for (name, res) in results.iter() {
-            test_vec.push((name, res));
-        }
-        self.print_test_result(name, test_vec);
+        self.print_test_result(name, &results);
     }
 
     /// Run all tests from all tests group
@@ -66,18 +62,14 @@ impl<'a> TestManager<'a> {
     }
 
     /// Run only selected tests
-    pub fn run_selected(&self, tests: Vec<(String, Option<Vec<&str>>)>) {
+    pub fn run_selected(&self, tests: Vec<(&str, Option<Vec<&str>>)>) {
         for (test_group_name, tests) in tests.iter() {
             if let Some(tg) = self.test_groups.get(test_group_name) {
                 match tests {
                     None => self.run_test_group(test_group_name, *tg),
                     Some(tests) => {
                         let results = tg.run_selected(tests);
-                        let mut test_vec = Vec::new();
-                        for (name, res) in results.iter() {
-                            test_vec.push((name, res));
-                        }
-                        self.print_test_result(test_group_name, test_vec);
+                        self.print_test_result(test_group_name, &results);
                     }
                 }
             } else {
