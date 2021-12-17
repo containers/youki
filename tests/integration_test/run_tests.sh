4965320d72de490b9f7fe2dfa130adbab1ef452d
utam0k <k0ma@utam0k.jp>
2021-12-18 00:03:52 +0900

    fix run_tests.sh

--- tests/integration_test/run_tests.sh
+++ tests/integration_test/run_tests.sh
@@ -3,18 +3,8 @@ set -e
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 RUNTIME=${1:-"$PROJECT_ROOT/youki"}
-INTEGRATION_TEST="$PROJECT_ROOT/integration_tests"
 
 cd $PROJECT_ROOT/tests/integration_test
-
-if [[ ! -e "$PROJECT_ROOT/$RUNTIME" ]]; then
-    current=$(pwd)
-    cd $PROJECT_ROOT
-    ./build.sh --release
-    cd $current
-fi
-
-cp $RUNTIME "$PROJECT_ROOT/tests/integration_test"
 
 TARGET=${TARGET-x86_64-unknown-linux-gnu}
 if [ "$TARGET" != "" ]; then
@@ -24,8 +14,8 @@ VERSION=debug
 if [ "$1" == "--release" ]; then
     VERSION=release
 fi
-cargo build --verbose $TGT $1
-cp target/$TARGET/$VERSION/integration_test ./youki_integration_test
+cargo build --verbose $TGT
+cp $PROJECT_ROOT/tests/integration_test/target/$TARGET/$VERSION/integration_test ./youki_integration_test
 
 logfile="./test_log.log"
 touch $logfile
