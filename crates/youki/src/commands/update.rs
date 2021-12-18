21eaf4e020b444446d8b339dc050cbab30eeeda9
Jian Zeng <anonymousknight96@gmail.com>
2021-12-18 23:56:40 +0800

    feat: add --resource option to update subcommand
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

--- crates/youki/src/commands/update.rs
+++ crates/youki/src/commands/update.rs
@@ -1,19 +1,29 @@
+use std::fs;
+use std::io;
 use std::path::PathBuf;
 
 use crate::commands::create_cgroup_manager;
 use anyhow::Result;
 use libcgroups::{self, common::ControllerOpt};
 use liboci_cli::Update;
-use oci_spec::runtime::{LinuxPids, LinuxResources};
+use oci_spec::runtime::{LinuxPidsBuilder, LinuxResources, LinuxResourcesBuilder};
 
 pub fn update(args: Update, root_path: PathBuf) -> Result<()> {
     let cmanager = create_cgroup_manager(root_path, &args.container_id)?;
 
-    let mut linux_res = LinuxResources::default();
-    if let Some(new_pids_limit) = args.pids_limit {
-        let mut pids = LinuxPids::default();
-        pids.set_limit(new_pids_limit);
-        linux_res.set_pids(Some(pids));
+    let linux_res: LinuxResources;
+    if let Some(resources_path) = args.resources {
+        linux_res = if resources_path.to_string_lossy() == "-" {
+            serde_json::from_reader(io::stdin())?
+        } else {
+            serde_json::from_reader(fs::File::open(resources_path)?)?
+        };
+    } else {
+        let mut builder = LinuxResourcesBuilder::default();
+        if let Some(new_pids_limit) = args.pids_limit {
+            builder = builder.pids(LinuxPidsBuilder::default().limit(new_pids_limit).build()?);
+        }
+        linux_res = builder.build()?;
     }
 
     cmanager.apply(&ControllerOpt {
