d0d756a02a6e417bb7194cbd805e6383feed1e43
Furisto <24721048+Furisto@users.noreply.github.com>
2022-01-14 22:07:33 +0100

    Ensure youki runs under podman

--- crates/youki/src/commands/delete.rs
+++ crates/youki/src/commands/delete.rs
@@ -1,4 +1,4 @@
-use crate::commands::load_container;
+use crate::commands::{container_exists, load_container};
 use anyhow::{Context, Result};
 use std::path::PathBuf;
 
@@ -6,6 +6,10 @@ use liboci_cli::Delete;
 
 pub fn delete(args: Delete, root_path: PathBuf) -> Result<()> {
     log::debug!("start deleting {}", args.container_id);
+    if !container_exists(&root_path, &args.container_id)? && args.force {
+        return Ok(());
+    }
+
     let mut container = load_container(root_path, &args.container_id)?;
     container
         .delete(args.force)
