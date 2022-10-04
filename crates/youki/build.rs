15978cae6d9e999fab78f7c0c1e370b0fb455f35
Yashodhan Joshi <yjdoc2@gmail.com>
2022-10-04 14:40:12 +0530

    Add vergen git commit sha override if .git not found

--- crates/youki/build.rs
+++ crates/youki/build.rs
@@ -4,5 +4,7 @@ use vergen::{vergen, Config, ShaKind};
 fn main() -> Result<()> {
     let mut config = Config::default();
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
+    *config.git_mut().skip_if_error_mut() = true;
+    println!("cargo:rustc-env=VERGEN_GIT_SHA_SHORT=unknown");
     vergen(config)
 }
