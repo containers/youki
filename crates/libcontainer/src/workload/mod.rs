e23e4416770e762fdd67f31a619544058228c7d7
Xin Liu <sam@secondstate.io>
2022-10-21 22:59:54 +0800

    feat(libcontainer): introduce WasmEdgeExecutor into ExecutorManager
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/libcontainer/src/workload/mod.rs
+++ crates/libcontainer/src/workload/mod.rs
@@ -1,9 +1,19 @@
 use anyhow::{Context, Result};
 use oci_spec::runtime::Spec;
 
-use self::wasmedge::WasmEdge;
+use self::default::DefaultExecutor;
+#[cfg(feature = "wasm-wasmedge")]
+use self::wasmedge::WasmEdgeExecutor;
+#[cfg(feature = "wasm-wasmer")]
+use self::wasmer::WasmerExecutor;
 
+pub mod default;
+#[cfg(feature = "wasm-wasmedge")]
 pub mod wasmedge;
+#[cfg(feature = "wasm-wasmer")]
+pub mod wasmer;
+
+static EMPTY: Vec<String> = Vec::new();
 
 pub trait Executor {
     /// Executes the workload
@@ -17,6 +27,16 @@ pub struct ExecutorManager {}
 
 impl ExecutorManager {
     pub fn exec(spec: &Spec) -> Result<()> {
-        return WasmEdge::exec(spec).context("wasmedge execution failed");
+        #[cfg(feature = "wasm-wasmer")]
+        if WasmerExecutor::can_handle(spec)? {
+            return WasmerExecutor::exec(spec).context("wasmer execution failed");
+        }
+
+        #[cfg(feature = "wasm-wasmedge")]
+        if WasmEdgeExecutor::can_handle(spec)? {
+            return WasmEdgeExecutor::exec(spec).context("wasmedge execution failed");
+        }
+
+        DefaultExecutor::exec(spec).context("default execution failed")
     }
 }
