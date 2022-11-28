371d87ab909abf0a6a2b41fc620da23360dad6cd
chermehdi <mehdi.cheracher@gmail.com>
2022-11-28 07:37:56 +0000

    Address comment issues.
    * Fix capitalisation in error messages.
    * Split tests into individual methods.
    
    Signed-off-by: chermehdi <mehdi.cheracher@gmail.com>

--- tests/rust-integration-tests/integration_test/src/tests/hostname/mod.rs
+++ tests/rust-integration-tests/integration_test/src/tests/hostname/mod.rs
@@ -1,11 +1,10 @@
-use anyhow::{Context, Result};
 use oci_spec::runtime::{LinuxBuilder, ProcessBuilder, Spec, SpecBuilder};
 use test_framework::{Test, TestGroup, TestResult};
 
 use crate::utils::test_inside_container;
 
-fn create_spec(hostname: &str) -> Result<Spec> {
-    Ok(SpecBuilder::default()
+fn create_spec(hostname: &str) -> Spec {
+    SpecBuilder::default()
         .hostname(hostname)
         .linux(
             LinuxBuilder::default()
@@ -15,39 +14,41 @@ fn create_spec(hostname: &str) -> Result<Spec> {
                     "/proc/sys".to_string(),
                 ])
                 .build()
-                .expect("should build linux config"),
+                .expect("error in building linux config"),
         )
         .process(
             ProcessBuilder::default()
                 .args(vec!["runtimetest".to_string()])
                 .build()
-                .expect("can create process"),
+                .expect("error in creating process config"),
         )
         .build()
-        .context("Failed to build hostname spec")?)
+        .unwrap()
 }
 
 fn hostname_test() -> TestResult {
-    let cases = vec!["hostname-specific", ""];
-    for case in cases {
-        let spec = create_spec(case).expect("should create spec");
-        let test_result = test_inside_container(spec, &|_| {
-            // As long as the container is created, we expect the hostname to be determined
-            // by the spec, so nothing to prepare prior.
-            Ok(())
-        });
-        // fail fast
-        if let TestResult::Failed(e) = test_result {
-            return TestResult::Failed(e);
-        }
-    }
-    TestResult::Passed
+    let spec = create_spec("hostname-specific");
+    test_inside_container(spec, &|_| {
+        // As long as the container is created, we expect the hostname to be determined
+        // by the spec, so nothing to prepare prior.
+        Ok(())
+    })
+}
+
+fn empty_hostname() -> TestResult {
+    let spec = create_spec("");
+    test_inside_container(spec, &|_| {
+        // As long as the container is created, we expect the hostname to be determined
+        // by the spec, so nothing to prepare prior.
+        Ok(())
+    })
 }
 
 pub fn get_hostname_test() -> TestGroup {
     let mut test_group = TestGroup::new("set_host_name");
     let hostname_test = Test::new("set_host_name_test", Box::new(hostname_test));
-    test_group.add(vec![Box::new(hostname_test)]);
+    let empty_hostname_test = Test::new("set_empty_host_name_test", Box::new(empty_hostname));
+    test_group.add(vec![Box::new(hostname_test), Box::new(empty_hostname_test)]);
 
     test_group
 }
