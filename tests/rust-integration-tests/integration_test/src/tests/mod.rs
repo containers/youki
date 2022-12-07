7757267d0ed6250e4831cb6128c95b1f18ebdccd
Uchio Kondo <udzura@udzura.jp>
2022-12-07 14:41:11 +0900

    Fix cargo fmt --all warnings
    Signed-off-by: Uchio Kondo <udzura@udzura.jp>

--- tests/rust-integration-tests/integration_test/src/tests/mod.rs
+++ tests/rust-integration-tests/integration_test/src/tests/mod.rs
@@ -1,9 +1,9 @@
 pub mod cgroups;
 pub mod hooks;
+pub mod hostname;
 pub mod lifecycle;
 pub mod linux_ns_itype;
 pub mod pidfile;
 pub mod readonly_paths;
 pub mod seccomp_notify;
 pub mod tlb;
-pub mod hostname;
