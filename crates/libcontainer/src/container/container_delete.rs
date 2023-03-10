44e296be63752eb034603227b8dab4441da02c6b
Eric Fang <yihuaf@unkies.org>
2023-03-10 06:40:37 +0100

    refactored delete
    Refactored the delete to match runc and crun. Now youki allow deletion
        of created containers.
    
    Signed-off-by: Eric Fang <yihuaf@unkies.org>

--- crates/libcontainer/src/container/container_delete.rs
+++ crates/libcontainer/src/container/container_delete.rs
@@ -27,51 +27,78 @@ impl Container {
     pub fn delete(&mut self, force: bool) -> Result<()> {
         self.refresh_status()
             .context("failed to refresh container status")?;
-        if self.can_kill() && force {
-            self.do_kill(signal::Signal::SIGKILL, true)?;
-            self.set_status(ContainerStatus::Stopped).save()?;
-        }
+
         log::debug!("container status: {:?}", self.status());
-        if self.can_delete() {
-            if self.root.exists() {
-                if let Ok(config) = YoukiConfig::load(&self.root) {
-                    log::debug!("config: {:?}", config);
 
-                    // remove the cgroup created for the container
-                    // check https://man7.org/linux/man-pages/man7/cgroups.7.html
-                    // creating and removing cgroups section for more information on cgroups
-                    let use_systemd = self
-                        .systemd()
-                        .context("container state does not contain cgroup manager")?;
-                    let cmanager = libcgroups::common::create_cgroup_manager(
-                        &config.cgroup_path,
-                        use_systemd,
-                        self.id(),
-                    )
-                    .context("failed to create cgroup manager")?;
-                    cmanager.remove().with_context(|| {
-                        format!("failed to remove cgroup {}", config.cgroup_path.display())
-                    })?;
-
-                    if let Some(hooks) = config.hooks.as_ref() {
-                        hooks::run_hooks(hooks.poststop().as_ref(), Some(self))
-                            .with_context(|| "failed to run post stop hooks")?;
-                    }
-                }
-
-                // remove the directory storing container state
-                log::debug!("remove dir {:?}", self.root);
-                fs::remove_dir_all(&self.root).with_context(|| {
-                    format!("failed to remove container dir {}", self.root.display())
-                })?;
+        // Check if container is allowed to be deleted based on container status.
+        match self.status() {
+            ContainerStatus::Stopped => {}
+            ContainerStatus::Created => {
+                // Here, we differ from the OCI spec, but matches the same
+                // behavior as `runc` and `crun`. The OCI spec does not allow
+                // deletion of status `created` without `force` flag. But both
+                // `runc` and `crun` allows deleting `created`. Therefore we
+                // decided to follow `runc` and `crun`. In the case where
+                // container is in `created` status, we first kill the container
+                // init process which is waiting on start signal. Since only a
+                // single container process (the init process) is created, we do
+                // not need to send signals to all in this case.
+                self.do_kill(signal::Signal::SIGKILL, false)?;
+                self.set_status(ContainerStatus::Stopped).save()?;
             }
-            Ok(())
-        } else {
-            bail!(
-                "{} could not be deleted because it was {:?}",
-                self.id(),
-                self.status()
-            )
+            ContainerStatus::Creating | ContainerStatus::Running | ContainerStatus::Paused => {
+                // Containers can't be deleted while in these status, unless
+                // force flag is set. In the force case, we need to clean up any
+                // processes launched by containers.
+                if force {
+                    self.do_kill(signal::Signal::SIGKILL, true)?;
+                    self.set_status(ContainerStatus::Stopped).save()?;
+                } else {
+                    bail!(
+                        "{} could not be deleted because it was {:?}",
+                        self.id(),
+                        self.status()
+                    )
+                }
+            }
         }
+
+        // Once reached here, the container is verified that it can be deleted.
+        debug_assert!(self.status().can_delete());
+
+        if self.root.exists() {
+            if let Ok(config) = YoukiConfig::load(&self.root) {
+                log::debug!("config: {:?}", config);
+
+                // remove the cgroup created for the container
+                // check https://man7.org/linux/man-pages/man7/cgroups.7.html
+                // creating and removing cgroups section for more information on cgroups
+                let use_systemd = self
+                    .systemd()
+                    .context("container state does not contain cgroup manager")?;
+                let cmanager = libcgroups::common::create_cgroup_manager(
+                    &config.cgroup_path,
+                    use_systemd,
+                    self.id(),
+                )
+                .context("failed to create cgroup manager")?;
+                cmanager.remove().with_context(|| {
+                    format!("failed to remove cgroup {}", config.cgroup_path.display())
+                })?;
+
+                if let Some(hooks) = config.hooks.as_ref() {
+                    hooks::run_hooks(hooks.poststop().as_ref(), Some(self))
+                        .with_context(|| "failed to run post stop hooks")?;
+                }
+            }
+
+            // remove the directory storing container state
+            log::debug!("remove dir {:?}", self.root);
+            fs::remove_dir_all(&self.root).with_context(|| {
+                format!("failed to remove container dir {}", self.root.display())
+            })?;
+        }
+
+        Ok(())
     }
 }
