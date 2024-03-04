a84dbefde0e9258b6e52deacad3ede4221306977
Yashodhan Joshi <yjdoc2@gmail.com>
2024-03-04 15:46:29 +0530

    Add tests in podman rootless for exec
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- tests/rootless-tests/run.sh
+++ tests/rootless-tests/run.sh
@@ -10,9 +10,20 @@ podman rm --force --ignore create-test # remove if existing
 podman create --runtime $runtime --name create-test hello-world
 log=$(podman start -a create-test)
 echo $log | grep "This message shows that your installation appears to be working correctly"
-podman rm create-test
+podman rm --force --ignore create-test
 
 rand=$(head -c 10 /dev/random | base64)
 
 log=$(podman run --runtime $runtime fedora echo "$rand")
 echo $log | grep $rand
+
+podman rm --force --ignore exec-test
+podman run -d --runtime $runtime --name exec-test busybox sleep 10m
+
+rand=$(head -c 10 /dev/random | base64)
+
+log=$(podman exec --runtime $runtime exec-test echo "$rand")
+echo $log | grep $rand
+
+podman kill exec-test
+podman rm --force --ignore exec-test
