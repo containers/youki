df22714ff35b1d9d23039aa823458cbc66b8ac80
utam0k <k0ma@utam0k.jp>
2024-03-16 14:31:11 +0900

    Init a seccomp project
    Signed-off-by: utam0k <k0ma@utam0k.jp>

---
+++ experiment/seccomp/src/instruction/inst.rs
@@ -0,0 +1,69 @@
+use std::os::raw::{c_uchar, c_uint, c_ushort};
+
+// https://docs.kernel.org/networking/filter.html#structure
+// <linux/filter.h>: sock_filter
+#[repr(C)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Instruction {
+    pub code: c_ushort,
+    pub offset_jump_true: c_uchar,
+    pub offset_jump_false: c_uchar,
+    pub multiuse_field: c_uint,
+}
+
+impl Instruction {
+    fn new(
+        code: c_ushort,
+        jump_true: c_uchar,
+        jump_false: c_uchar,
+        multiuse_field: c_uint,
+    ) -> Self {
+        Instruction {
+            code,
+            offset_jump_true: jump_true,
+            offset_jump_false: jump_false,
+            multiuse_field,
+        }
+    }
+
+    pub fn jump(
+        code: c_ushort,
+        multiuse_field: c_uint,
+        jump_true: c_uchar,
+        jump_false: c_uchar,
+    ) -> Self {
+        Self::new(code, jump_true, jump_false, multiuse_field)
+    }
+
+    pub fn stmt(code: c_ushort, k: c_uint) -> Self {
+        Self::new(code, 0, 0, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::instruction::*;
+
+    #[test]
+    fn test_bpf_instructions() {
+        assert_eq!(
+            Instruction::stmt(BPF_LD | BPF_W | BPF_ABS, 16),
+            Instruction {
+                code: 0x20,
+                offset_jump_true: 0,
+                offset_jump_false: 0,
+                multiuse_field: 16,
+            }
+        );
+        assert_eq!(
+            Instruction::jump(BPF_JMP | BPF_JEQ | BPF_K, 10, 2, 5),
+            Instruction {
+                code: 0x15,
+                offset_jump_true: 2,
+                offset_jump_false: 5,
+                multiuse_field: 10,
+            }
+        );
+    }
+}
---
+++ experiment/seccomp/src/main.rs
@@ -0,0 +1,125 @@
+use nix::unistd::mkdir;
+use seccomp::instruction::*;
+use seccomp::seccomp::{NotifyFd, Seccomp};
+
+use std::io::{IoSlice, IoSliceMut};
+use std::os::fd::{IntoRawFd, OwnedFd};
+use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
+use std::slice;
+
+use anyhow::Result;
+use nix::{
+    libc,
+    sys::{
+        socket::{
+            self, ControlMessage, ControlMessageOwned, MsgFlags, SockFlag, SockType, UnixAddr,
+        },
+        stat::Mode,
+    },
+    unistd::close,
+};
+
+fn send_fd<F: AsRawFd>(sock: OwnedFd, fd: &F) -> nix::Result<()> {
+    let fd = fd.as_raw_fd();
+    let cmsgs = [ControlMessage::ScmRights(slice::from_ref(&fd))];
+
+    let iov = [IoSlice::new(b"x")];
+
+    socket::sendmsg::<()>(sock.into_raw_fd(), &iov, &cmsgs, MsgFlags::empty(), None)?;
+    Ok(())
+}
+
+fn recv_fd<F: FromRawFd>(sock: RawFd) -> nix::Result<Option<F>> {
+    let mut iov_buf = [];
+    let mut iov = [IoSliceMut::new(&mut iov_buf)];
+
+    let mut cmsg_buf = nix::cmsg_space!(RawFd);
+    let msg = socket::recvmsg::<UnixAddr>(sock, &mut iov, Some(&mut cmsg_buf), MsgFlags::empty())?;
+    match msg.cmsgs().next() {
+        Some(ControlMessageOwned::ScmRights(fds)) if fds.len() > 0 => {
+            let fd = unsafe { F::from_raw_fd(fds[0]) };
+            Ok(Some(fd))
+        }
+        _ => Ok(None),
+    }
+}
+
+fn handle_notifications(notify_fd: NotifyFd) -> nix::Result<()> {
+    loop {
+        println!("Waiting on next");
+        let req = notify_fd.recv()?.notif;
+        assert_eq!(req.data.nr, libc::SYS_mkdir as i32);
+        println!(
+            "Got notification for mkdir(2): id={}, pid={}, nr={}",
+            req.id, req.pid, req.data.nr
+        );
+    }
+}
+
+fn main() -> Result<()> {
+    let (sock_for_child, sock_for_parent) = socket::socketpair(
+        socket::AddressFamily::Unix,
+        SockType::Stream,
+        None,
+        SockFlag::empty(),
+    )?;
+
+    let _ = prctl::set_no_new_privileges(true);
+
+    let mut bpf_prog = Arch::X86.gen_validate();
+    bpf_prog.append(&mut vec![
+        // A: Check if syscall is getpid
+        Instruction::stmt(BPF_LD | BPF_W | BPF_ABS, 0),
+        Instruction::jump(BPF_JMP | BPF_JEQ | BPF_K, libc::SYS_getcwd as u32, 0, 1), // If false, go to B
+        Instruction::stmt(BPF_RET | BPF_K, SECCOMP_RET_KILL_PROCESS),
+        // B: Check if syscall is mkdir and if so, return seccomp notify
+        Instruction::stmt(BPF_LD | BPF_W | BPF_ABS, 0),
+        Instruction::jump(BPF_JMP | BPF_JEQ | BPF_K, libc::SYS_mkdir as u32, 0, 1), // If false, go to C
+        Instruction::stmt(BPF_RET | BPF_K, SECCOMP_RET_USER_NOTIF),
+        // C: Pass
+        Instruction::stmt(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+    ]);
+
+    let seccomp = Seccomp { filters: bpf_prog };
+
+    if let nix::unistd::ForkResult::Child = unsafe { nix::unistd::fork()? } {
+        // nix::unistd::ForkResult::Parent { child } => match wait::waitpid(child, None)? {
+        //     wait::WaitStatus::Signaled(_, signal, _) => {
+        //         if signal == Signal::SIGSYS {
+        //             println!("Got SIGSYS, seccomp filter applied successfully!");
+        //             return Ok(());
+        //         }
+        //         dbg!(signal);
+        //     }
+        //     wait_status => {
+        //         dbg!("Unexpected wait status: {:?}", wait_status);
+        //     }
+        // },
+        std::panic::catch_unwind(|| {
+            let notify_fd = seccomp.apply().unwrap();
+            println!(
+                "Seccomp applied successfully with notify fd: {:?}",
+                notify_fd
+            );
+            send_fd(sock_for_child, &notify_fd).unwrap();
+
+            if let Err(e) = mkdir("/tmp/test", Mode::S_IRUSR | Mode::S_IWUSR) {
+                eprintln!("Failed to mkdir: {}", e);
+            } else {
+                println!("mkdir succeeded");
+            }
+        })
+        .unwrap();
+
+        std::process::exit(0);
+    };
+
+    let notify_fd = recv_fd::<NotifyFd>(sock_for_parent.as_raw_fd())?.unwrap();
+
+    close(sock_for_child.as_raw_fd())?;
+    close(sock_for_parent.as_raw_fd())?;
+
+    handle_notifications(notify_fd)?;
+
+    Ok(())
+}
