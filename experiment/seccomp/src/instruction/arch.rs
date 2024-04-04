13d791b8e1bc1a420413a1c2404edb8ff63358b6
Toru Komatsu <k0ma@utam0k.jp>
2024-04-04 21:32:39 +0900

    Init a seccomp project (#2729)
    * Init a seccomp project
    
    Signed-off-by: utam0k <k0ma@utam0k.jp>
    
    * Make gen_validation a static method
    
    Signed-off-by: utam0k <k0ma@utam0k.jp>
    
    ---------
    
    Signed-off-by: utam0k <k0ma@utam0k.jp>

---
+++ experiment/seccomp/src/instruction/arch.rs
@@ -0,0 +1,18 @@
+use crate::instruction::Instruction;
+use crate::instruction::*;
+
+pub enum Arch {
+    X86,
+}
+
+pub fn gen_validate(arc: &Arch) -> Vec<Instruction> {
+    let arch = match arc {
+        Arch::X86 => AUDIT_ARCH_X86_64,
+    };
+
+    vec![
+        Instruction::stmt(BPF_LD | BPF_W | BPF_ABS, SECCOMP_DATA_ARCH_OFFSET as u32),
+        Instruction::jump(BPF_JMP | BPF_JEQ | BPF_K, 1, 0, arch),
+        Instruction::stmt(BPF_RET | BPF_K, SECCOMP_RET_KILL_PROCESS),
+    ]
+}
