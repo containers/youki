273f2b4932a9ef5222d873b556888398eaade7d1
Yashodhan Joshi <yjdoc2@gmail.com>
2024-04-10 11:43:57 +0530

    Remove unused pub items from contest util mod
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- tests/contest/contest/src/utils/mod.rs
+++ tests/contest/contest/src/utils/mod.rs
@@ -1,12 +1,11 @@
 pub mod support;
 pub mod test_utils;
-#[allow(unused_imports)]
+
 pub use support::{
-    generate_uuid, get_project_path, get_runtime_path, get_runtimetest_path, is_runtime_runc,
-    prepare_bundle, set_config, set_runtime_path,
+    generate_uuid, get_runtime_path, get_runtimetest_path, is_runtime_runc, prepare_bundle,
+    set_config,
 };
-#[allow(unused_imports)]
 pub use test_utils::{
     create_container, delete_container, get_state, kill_container, test_inside_container,
-    test_outside_container, ContainerData, State,
+    test_outside_container, State,
 };
