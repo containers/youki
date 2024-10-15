4ef6e4fd881803751d0cde27dd4e8a7ec2ead78d
nayuta-ai <nayuta723@gmail.com>
2024-10-15 23:50:58 +0900

    fix the test coverage issues and resolve the lint errors
    Signed-off-by: nayuta-ai <nayuta723@gmail.com>

--- tests/contest/runtimetest/src/tests.rs
+++ tests/contest/runtimetest/src/tests.rs
@@ -781,12 +781,13 @@ pub fn validate_masked_paths(spec: &Spec) {
     let masked_paths = match linux.masked_paths() {
         Some(p) => p,
         None => {
-            eprintln!("in readonly paths, expected some readonly paths to be set, found none");
+            eprintln!("in masked paths, expected some masked paths to be set, found none");
             return;
         }
     };
 
     if masked_paths.is_empty() {
+        eprintln!("in masked paths, expected some masked paths to be set, found none");
         return;
     }
 
@@ -802,7 +803,8 @@ pub fn validate_masked_paths(spec: &Spec) {
                 return;
             }
         } else {
-            /* Expected */
+            eprintln!("in masked paths, error in testing read access for path {path}");
+            return;
         }
     }
 }
