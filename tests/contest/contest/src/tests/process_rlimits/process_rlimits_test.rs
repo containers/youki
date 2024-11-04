a1b25421c231642af6aa092b2006fbbde6e73f4d
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-11-04 14:44:38 +0900

    fix newline code
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>

--- tests/contest/contest/src/tests/process_rlimits/process_rlimits_test.rs
+++ tests/contest/contest/src/tests/process_rlimits/process_rlimits_test.rs
@@ -1,33 +1,47 @@
 use crate::utils::test_inside_container;
 use anyhow::{Context, Ok, Result};
-use oci_spec::runtime::{PosixRlimit, PosixRlimitBuilder, PosixRlimitType, ProcessBuilder, Spec, SpecBuilder};
+use oci_spec::runtime::{
+    PosixRlimit, PosixRlimitBuilder, PosixRlimitType, ProcessBuilder, Spec, SpecBuilder,
+};
 use test_framework::{test_result, Test, TestGroup, TestResult};
 
 const GIGABYTES: u64 = 1024 * 1024 * 1024;
 
-fn create_rlimit(rlimit_type: PosixRlimitType, hard_val: u64, soft_val: u64) -> Result<PosixRlimit> {
-    let rlimit = PosixRlimitBuilder::default().
-        typ(rlimit_type)
+fn create_rlimit(
+    rlimit_type: PosixRlimitType,
+    hard_val: u64,
+    soft_val: u64,
+) -> Result<PosixRlimit> {
+    let rlimit = PosixRlimitBuilder::default()
+        .typ(rlimit_type)
         .hard(hard_val)
-        .soft(soft_val).build()?;
+        .soft(soft_val)
+        .build()?;
     Ok(rlimit)
 }
 
 fn create_spec() -> Result<Spec> {
-    let spec = SpecBuilder::default().process(
-        ProcessBuilder::default()
-            .rlimits(vec![
-                create_rlimit(PosixRlimitType::RlimitAs, 2*GIGABYTES, 1*GIGABYTES).unwrap(),
-                create_rlimit(PosixRlimitType::RlimitCore, 4*GIGABYTES, 3*GIGABYTES).unwrap(),
-                create_rlimit(PosixRlimitType::RlimitData, 6*GIGABYTES, 5*GIGABYTES).unwrap(),
-                create_rlimit(PosixRlimitType::RlimitFsize, 8*GIGABYTES, 7*GIGABYTES).unwrap(),
-                create_rlimit(PosixRlimitType::RlimitStack, 10*GIGABYTES, 9*GIGABYTES).unwrap(),
-                create_rlimit(PosixRlimitType::RlimitCpu, 120, 60).unwrap(),
-                create_rlimit(PosixRlimitType::RlimitNofile, 4000, 3000).unwrap(),
-            ])
-            .build()
-            .expect("error in creating process config"),
-    ).build().context("failed to build spec")?;
+    let spec = SpecBuilder::default()
+        .process(
+            ProcessBuilder::default()
+                .rlimits(vec![
+                    create_rlimit(PosixRlimitType::RlimitAs, 2 * GIGABYTES, 1 * GIGABYTES).unwrap(),
+                    create_rlimit(PosixRlimitType::RlimitCore, 4 * GIGABYTES, 3 * GIGABYTES)
+                        .unwrap(),
+                    create_rlimit(PosixRlimitType::RlimitData, 6 * GIGABYTES, 5 * GIGABYTES)
+                        .unwrap(),
+                    create_rlimit(PosixRlimitType::RlimitFsize, 8 * GIGABYTES, 7 * GIGABYTES)
+                        .unwrap(),
+                    create_rlimit(PosixRlimitType::RlimitStack, 10 * GIGABYTES, 9 * GIGABYTES)
+                        .unwrap(),
+                    create_rlimit(PosixRlimitType::RlimitCpu, 120, 60).unwrap(),
+                    create_rlimit(PosixRlimitType::RlimitNofile, 4000, 3000).unwrap(),
+                ])
+                .build()
+                .expect("error in creating process config"),
+        )
+        .build()
+        .context("failed to build spec")?;
 
     Ok(spec)
 }
@@ -38,10 +52,10 @@ fn process_rlimits_test() -> TestResult {
 }
 
 pub fn get_process_rlimits_test() -> TestGroup {
-    let mut process_test_group = TestGroup::new("process_rlimits");
+    let mut process_rlimits_test_group = TestGroup::new("process_rlimits");
 
     let test = Test::new("process_rlimits_test", Box::new(process_rlimits_test));
-    process_test_group.add(vec![Box::new(test)]);
+    process_rlimits_test_group.add(vec![Box::new(test)]);
 
-    process_test_group
+    process_rlimits_test_group
 }
