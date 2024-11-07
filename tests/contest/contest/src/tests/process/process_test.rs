988b9330955e0e1caf88befdd2fdcd9c0ec24111
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-11-08 07:47:10 +0900

    set process args to run test
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>

--- tests/contest/contest/src/tests/process/process_test.rs
+++ tests/contest/contest/src/tests/process/process_test.rs
@@ -1,14 +1,17 @@
-use crate::utils::test_inside_container;
-use anyhow::{bail, Context, Ok, Result};
-use oci_spec::runtime::{ProcessBuilder, Spec, SpecBuilder};
 use std::fs;
 use std::os::unix::fs::PermissionsExt;
+
+use anyhow::{bail, Context, Ok, Result};
+use oci_spec::runtime::{ProcessBuilder, Spec, SpecBuilder};
 use test_framework::{test_result, Test, TestGroup, TestResult};
+
+use crate::utils::test_inside_container;
 
 fn create_spec() -> Result<Spec> {
     let spec = SpecBuilder::default()
         .process(
             ProcessBuilder::default()
+                .args(vec!["runtimetest".to_string(), "process".to_string()])
                 .cwd("/test")
                 .env(vec!["testa=valuea".into(), "testb=123".into()])
                 .build()
@@ -22,14 +25,16 @@ fn create_spec() -> Result<Spec> {
 
 fn process_test() -> TestResult {
     let spec = test_result!(create_spec());
+    let binding = spec.process().clone().unwrap();
+    let cwd = binding.cwd();
     test_inside_container(spec, &|_| {
-        match fs::create_dir("/test") {
+        match fs::create_dir(cwd) {
             Result::Ok(_) => { /*This is expected*/ }
             Err(e) => {
                 bail!(e)
             }
         }
-        let metadata = fs::metadata("/test")?;
+        let metadata = fs::metadata(cwd)?;
         let mut permissions = metadata.permissions();
         permissions.set_mode(0o700);
 
