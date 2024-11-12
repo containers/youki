5a9e78fe6506bb934e8bec3099d914f39056d218
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-11-12 22:11:34 +0900

    Add e2e test: process rlimits (#2977)
    * add process rlimits test
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>
    
    * fix newline code
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>
    
    * fix fmt
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>
    
    * fix format err
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>
    
    * add process args to run test
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>
    
    * fix unsafe code by use nix
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>
    
    * remove unused import
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>

--- tests/contest/runtimetest/src/main.rs
+++ tests/contest/runtimetest/src/main.rs
@@ -44,6 +44,7 @@ fn main() {
         "io_priority_class_be" => tests::test_io_priority_class(&spec, IoprioClassBe),
         "io_priority_class_idle" => tests::test_io_priority_class(&spec, IoprioClassIdle),
         "devices" => tests::validate_devices(&spec),
+        "process_rlimits" => tests::validate_process_rlimits(&spec),
         "no_pivot" => tests::validate_rootfs(),
         _ => eprintln!("error due to unexpected execute test name: {execute_test}"),
     }
