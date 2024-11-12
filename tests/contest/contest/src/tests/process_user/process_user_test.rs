31977e4a1c86b8570d9f8456ac1a202913eae0bb
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-11-13 08:51:21 +0900

    set additional_gids to random number
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>

--- tests/contest/contest/src/tests/process_user/process_user_test.rs
+++ tests/contest/contest/src/tests/process_user/process_user_test.rs
@@ -1,15 +1,24 @@
 use anyhow::{Context, Ok, Result};
 use oci_spec::runtime::{ProcessBuilder, Spec, SpecBuilder, UserBuilder};
+use rand::Rng;
 use test_framework::{test_result, Test, TestGroup, TestResult};
 
 use crate::utils::test_inside_container;
+
+// Generates a Vec<u32> with a random number of elements (between 5 and 15),
+// where each element is a random u32 value between 0 and 65535.
+fn generate_random_vec() -> Vec<u32> {
+    let mut rng = rand::thread_rng();
+    let vec_size = rng.gen_range(5..=15);
+    (0..vec_size).map(|_| rng.gen_range(0..=65535)).collect()
+}
 
 fn create_spec() -> Result<Spec> {
     let user = UserBuilder::default()
         .uid(10u32)
         .gid(10u32)
-        .additional_gids(vec![5u32])
-        .umask(0o02u32)
+        .additional_gids(generate_random_vec())
+        .umask(u32::from(0o002))
         .build()?;
 
     let spec = SpecBuilder::default()
