6b18bffd148947f15f4e9f6248504a67d6de0f5b
Yashodhan Joshi <yjdoc2@gmail.com>
2024-11-14 19:21:10 +0530

    fix: make sure random gid values are unique
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- tests/contest/contest/src/tests/process_user/process_user_test.rs
+++ tests/contest/contest/src/tests/process_user/process_user_test.rs
@@ -7,10 +7,17 @@ use crate::utils::test_inside_container;
 
 // Generates a Vec<u32> with a random number of elements (between 5 and 15),
 // where each element is a random u32 value between 0 and 65535.
-fn generate_random_vec() -> Vec<u32> {
+fn generate_unique_random_vec() -> Vec<u32> {
     let mut rng = rand::thread_rng();
     let vec_size = rng.gen_range(5..=10);
-    (0..vec_size).map(|_| rng.gen_range(100..=200)).collect()
+    let mut ret = Vec::new();
+    while ret.len() < vec_size {
+        let rand = rng.gen_range(100..=200);
+        if !ret.contains(&rand) {
+            ret.push(rand);
+        }
+    }
+    ret
 }
 
 fn create_spec() -> Result<Spec> {
@@ -18,7 +25,7 @@ fn create_spec() -> Result<Spec> {
     let user = UserBuilder::default()
         .uid(10u32)
         .gid(10u32)
-        .additional_gids(generate_random_vec())
+        .additional_gids(generate_unique_random_vec())
         .umask(umask as u32)
         .build()?;
 
